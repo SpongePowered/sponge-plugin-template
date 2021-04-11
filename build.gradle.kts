096a5493bcf19746e871f2ee67a69393d173f4bc
zml <zml@stellardrift.ca>
2021-04-10 19:43:05 -0700

    Bump for Gradle 7 and SpongeGradle 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.spongepowered.plugin.metadata.PluginDependency
 
 plugins {
     `java-library`
-    id("org.spongepowered.gradle.plugin") version "1.0.2"
+    id("org.spongepowered.gradle.plugin") version "1.0.3"
 }
 
 group = "org.spongepowered"
