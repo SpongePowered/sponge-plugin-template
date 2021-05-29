414887b33414f525a911736d255fd82e4b2f6045
zml <zml@stellardrift.ca>
2021-05-28 22:01:43 -0700

    bump gradle, SpongeGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.spongepowered.plugin.metadata.PluginDependency
 
 plugins {
     `java-library`
-    id("org.spongepowered.gradle.plugin") version "1.0.3"
+    id("org.spongepowered.gradle.plugin") version "1.1.1"
 }
 
 group = "org.spongepowered"
