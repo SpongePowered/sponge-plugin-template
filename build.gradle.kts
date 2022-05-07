a9a0682ecbdbc2a629504aa44ab5f388cb20688c
zml <zml@stellardrift.ca>
2022-05-07 12:21:37 -0700

    build: Update gradle, actions, SG

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.spongepowered.plugin.metadata.model.PluginDependency
 
 plugins {
     `java-library`
-    id("org.spongepowered.gradle.plugin") version "2.0.1"
+    id("org.spongepowered.gradle.plugin") version "2.0.2"
 }
 
 group = "org.spongepowered"
