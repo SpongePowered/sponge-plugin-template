5544b3757d88186111369e8e63b10c38bfc3703b
zml <zml@stellardrift.ca>
2022-05-06 18:55:48 -0700

    chore: Bump SG, bump gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.spongepowered.plugin.metadata.model.PluginDependency
 
 plugins {
     `java-library`
-    id("org.spongepowered.gradle.plugin") version "2.0.0"
+    id("org.spongepowered.gradle.plugin") version "2.0.1"
 }
 
 group = "org.spongepowered"
